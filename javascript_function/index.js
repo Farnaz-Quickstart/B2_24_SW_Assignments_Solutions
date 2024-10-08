3fa94143e880455a76631a035063ed292bcc916f
farnaz <farnaz.towhidi@gmail.com>
2024-10-08 17:59:54 -0400

    Update assiignment functions

---
+++ javascript_function/index.js
@@ -0,0 +1,26 @@
+
+
+function handleAddition () {
+  let number1 = Number(document.getElementById ("inputNumber1").value);
+  let number2 = Number(document.getElementById ("inputNumber2").value)
+  document.getElementById ("inputResult").value = number1 + number2;
+}
+
+function handleSubtraction () {
+  let number1 = Number(document.getElementById ("inputNumber1").value);
+  let number2 = Number(document.getElementById ("inputNumber2").value)
+  document.getElementById ("inputResult").value = number1 - number2;
+}
+
+function handleMultipication () {
+  let number1 = Number(document.getElementById ("inputNumber1").value);
+  let number2 = Number(document.getElementById ("inputNumber2").value)
+  document.getElementById ("inputResult").value = number1 * number2;
+}
+
+function handleDivision () {
+  let number1 = Number(document.getElementById ("inputNumber1").value);
+  let number2 = Number(document.getElementById ("inputNumber2").value)
+  document.getElementById ("inputResult").value = number1 / number2;
+}
+
