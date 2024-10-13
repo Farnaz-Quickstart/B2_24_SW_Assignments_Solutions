ba432ad23d2f633e858fb4f15ba38b7f3b765c1b
farnaz <farnaz.towhidi@gmail.com>
2024-10-13 18:46:53 -0400

    update style

--- javascript_multipication/index.js
+++ javascript_multipication/index.js
@@ -1,13 +1,17 @@
 function generateTable () {
 
-
   let inputNumber = document.getElementById("input_number").value;
   let input_result = document.getElementById("input_result");
-  console.log (input_result);
 
   for (let i = 0 ; i <= 10 ; i++) {
-    console.log (`${inputNumber} X ${i} = ${i*inputNumber}`)
-    input_result.innerHTML +=`${inputNumber} X ${i} = ${i*inputNumber} <br>`
+    console.log (i*inputNumber)
+    if ((i*inputNumber % 2 ) == 0) 
+        input_result.innerHTML +=`${inputNumber} X ${i} = ${i*inputNumber} <br>`
+    else
+      input_result.innerHTML +=`<span style="color:red">${inputNumber} X ${i} = ${i*inputNumber} </span><br>`
+
+    
+  
   }
  
 } 
