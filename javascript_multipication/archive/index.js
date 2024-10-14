4e86962a7150bfe877527646a8bb567d26fba99c
farnaz <farnaz.towhidi@gmail.com>
2024-10-14 19:41:30 -0400

    multipication table

--- javascript_multipication/archive/index.js
+++ javascript_multipication/archive/index.js
@@ -4,14 +4,15 @@ function generateTable () {
   let input_result = document.getElementById("input_result");
 
   for (let i = 0 ; i <= 10 ; i++) {
-    console.log (i*inputNumber)
+    inputname = `result${i}`
+    document.getElementById(inputname).value = i
+
     if ((i*inputNumber % 2 ) == 0) 
         input_result.innerHTML +=`${inputNumber} X ${i} = ${i*inputNumber} <br>`
     else
       input_result.innerHTML +=`<span style="color:red">${inputNumber} X ${i} = ${i*inputNumber} </span><br>`
-
-    
-  
   }
+
+
  
 } 
---
+++ javascript_multipication/script.js
@@ -0,0 +1,14 @@
+function generateMultipication () {
+  let inputValue = document.getElementById("input_number").value
+  let inputResult = document.getElementById("result")
+  let inputResult1 = document.getElementById("result1")
+  console.log (inputResult1)
+  inputResult1.value = "<br>This is a test <br>" 
+
+
+  for (let i = 0 ; i <=10 ; i++) {
+    let result = inputValue * i
+    inputResult.innerHTML += `${inputValue} X ${i} = ${result} <br>`
+  }
+
+}
