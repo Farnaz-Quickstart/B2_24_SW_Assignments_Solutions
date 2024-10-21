71d313684d8103fb9cd1eb3aba53ba567224f899
farnaz <farnaz.towhidi@gmail.com>
2024-10-20 21:33:43 -0400

    add product list assignment

--- javascript_function_calculator/index.js
+++ javascript_function_calculator/index.js
@@ -1,8 +1,11 @@
 
 function handleAddition () {
+  
+ 
   let number1 = Number(document.getElementById("number1").value)
   let number2 = Number(document.getElementById("number2").value)
   document.getElementById("result").value = number1 + number2
+
 }
 
 function handleSubtraction () {
@@ -21,6 +24,15 @@ function handleDivision () {
   let number1 = Number(document.getElementById("number1").value)
   let number2 = Number(document.getElementById("number2").value)
   document.getElementById("result").value = number1 / number2
+}
+
+
+let myvar1 = "10"
+let dayofWeek = ["sat", "sun", "Mon", "Thu"]
+console.log (myvar1)
+console.log (dayofWeek[0])
+for (let i = 0 ; i < dayofWeek.length; i++) {
+  console.log (dayofWeek[i])
 }
 
 
